ef6bfefeb575441b0cbf0020ebd899f9347dfc87
Per Helge Vollmo <per.helge.vollmo@gmail.com>
2023-01-03 19:39:06 +0100

    implement add todo items

--- web.py
+++ web.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state['new_todo'] +'\n' # session state type: form of dict
+    todos.append(todo)
+    functions.write_todos(todos)
+
 todos = functions.get_todos()
 
 st.title('Min Todo-App')
@@ -10,4 +15,8 @@ st.write('Økt produktivitet - Bedre kompetanse i Python')
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input('', placeholder='Legg inn et gjøremål')
+st.text_input(label='', placeholder='Legg inn et gjøremål', 
+on_change=add_todo, key='new_todo')
+
+
+st.session_state
