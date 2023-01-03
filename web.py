0a74aa9495ecd0e4b2e688d7167e92c2b970e61d
Per Helge Vollmo <per.helge.vollmo@gmail.com>
2023-01-03 19:54:05 +0100

    Gjøre ferdig - funksjon for ferdigsdtilling

--- web.py
+++ web.py
@@ -6,17 +6,24 @@ def add_todo():
     todos.append(todo)
     functions.write_todos(todos)
 
+
+
 todos = functions.get_todos()
 
-st.title('Min Todo-App')
+st.title('Mine oppgaver')
 st.subheader('Min første web-app')
-st.write('Økt produktivitet - Bedre kompetanse i Python')
+st.write('Laget med Python')
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
 
 st.text_input(label='', placeholder='Legg inn et gjøremål', 
 on_change=add_todo, key='new_todo')
 
-
-st.session_state
