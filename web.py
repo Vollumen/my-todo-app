8f9a08ca535217e0b58a130c36923d926057dee6
Per Helge Vollmo <per.helge.vollmo@gmail.com>
2023-01-03 19:12:57 +0100

    web.py leser nå til dosos.txt

--- web.py
+++ web.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import functions
+
+todos = functions.get_todos()
 
 st.title('Min Todo-App')
+st.subheader('Min første web-app')
+st.write('Økt produktivitet - Bedre kompetanse i Python')
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input('', placeholder='Legg inn et gjøremål')
